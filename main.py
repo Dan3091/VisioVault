9115ad4617d98d721f9329e9fd0978c8d190d04c
Dan <clarc.2007@gmail.com>
2024-01-23 18:49:46 +0200

    Created validations for all entries

--- main.py
+++ main.py
@@ -21,6 +21,46 @@ app_tags = {"Facebook": ["/html/body/div[3]/div[2]/div/div/div/div/div[4]/button
 
 database = {}
 
+def validate_app_name():
+    """
+    This function validates the select_app() function,
+    if nothing is selected it returns False otherwise True
+    """
+
+    if select_app() == None:
+        popup_message(f"Please, first select the app!")
+        return False
+    else:
+        return True
+
+def validate_email():
+    """
+    This function validates the email_entry, if app.email_entry.get() return an empty string,
+     then it change the text from email_error label, and returns False, otherwise it returns True.
+     """
+
+    email = app.email_entry.get()
+    if len(email) == 0:
+        app.email_error.config(text="*Fill the Email")
+        return False
+    else:
+        app.email_error.config(text="")
+        return True
+
+def validate_password():
+    """
+    This function validates the password_entry, if app.password_entry.get() return an empty string,
+    then it change the text from password_error label, and returns False, otherwise it returns True.
+    """
+
+    password = app.password_entry.get()
+    if len(password) == 0:
+        app.password_error.config(text="*Fill the Password")
+        return False
+    else:
+        app.password_error.config(text="")
+        return True
+
 def select_app():
     """
     app.var.get() return the selected radio button 1 or 2,
