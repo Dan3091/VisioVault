e651ef8960cad4cc7f696679f5518f15c83df4c5
Dan <clarc.2007@gmail.com>
2024-01-23 14:03:44 +0200

    Initialized the future functions names in the constructor

--- gui.py
+++ gui.py
@@ -6,7 +6,13 @@ class App(Tk):
     This class is used to define the main window by using tkinter graphic interface module
     """
 
-    def __init__(self):
+    def __init__(self, add_app, reset, start, face_id_security, select_app):
+        self.add_app = add_app
+        self.reset = reset
+        self.start = start
+        self.face_id_security = face_id_security
+        self.select_app = select_app
+
         super().__init__()
         self.configure(bg='#8e8e8e')
         self.title("VisioVault")
